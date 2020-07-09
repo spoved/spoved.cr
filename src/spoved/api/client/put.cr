237a8ac257a69321b9bee996bd9c78f24d376b9f
Holden Omans <holden.omans@gmail.com>
2020-07-09 18:03:47 -0400

    update

--- src/spoved/api/client/put.cr
+++ src/spoved/api/client/put.cr
@@ -1,7 +1,7 @@
 module Spoved
   class Api
     class Client
-      # Make a POST request
+      # Make a PUT request
       def put(path : String, body = "", params : String | Nil = nil, klass : Class = JSON::Any,
               extra_headers : Hash(String, String)? = nil)
         resp = put_raw(path, body, params, extra_headers)
@@ -18,6 +18,42 @@ module Spoved
       def put_raw(path : String, body = "", params : String | Nil = nil,
                   extra_headers : Hash(String, String)? = nil)
         make_put_request(make_request_uri(path, params), body, extra_headers)
+      end
+
+      def put_file(path : String, file : File, extra_headers : Hash(String, String)? = nil)
+        uri = make_request_uri(path)
+        self.logger.debug { "PUT FILE: #{uri.to_s}" }
+
+        headers = HTTP::Headers.new
+        (extra_headers.nil? ? default_headers : default_headers.merge(extra_headers)).each do |k, v|
+          headers[k] = v
+        end
+
+        fio = File.open(file.path)
+        resp = HTTP::Client.put(url: uri.to_s, body: fio, headers: headers, tls: tls)
+        fio.close
+
+        logger.trace { resp.body }
+        resp
+      rescue e
+        logger.error { resp.inspect }
+        logger.error { e }
+        raise e
+      end
+
+      def put_form(path : String, form, extra_headers : Hash(String, String)? = nil)
+        uri = make_request_uri(path)
+        self.logger.debug { "PUT FORM: #{uri.to_s}" }
+
+        headers = extra_headers.nil? ? default_headers : default_headers.merge(extra_headers)
+        resp = halite.put(uri.to_s, form: form, headers: headers, tls: tls)
+
+        logger.trace { resp.body }
+        resp
+      rescue e
+        logger.error { resp.inspect }
+        logger.error { e }
+        raise e
       end
 
       private def make_put_request(uri : URI, body = "", extra_headers : Hash(String, String)? = nil)
