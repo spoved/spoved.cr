3de4af9bf29d3dfd0693f4e89360a301acc907ec
Holden Omans <holden.omans@gmail.com>
2021-04-14 10:19:41 -0400

    added enum_converter macro

--- src/spoved/macros.cr
+++ src/spoved/macros.cr
@@ -18,3 +18,24 @@ macro enum_to_const(name, _enum)
     {% end %}
   ]
 end
+
+macro enum_converter(e)
+  {% klass = e.resolve %}
+  struct ::{{klass.id}}Converter
+    def self.to_bson(value)
+      value.to_s.downcase
+    end
+
+    def self.from_bson(bson)
+      ::{{klass.id}}.from_s(bson.to_s)
+    end
+
+    def self.from_json(pull : JSON::PullParser)
+      ::{{klass.id}}.from_s(pull.read_string)
+    end
+
+    def self.to_json(value : ::{{klass.id}}, json : JSON::Builder)
+      value.to_s.downcase.to_json(json)
+    end
+  end
+end
