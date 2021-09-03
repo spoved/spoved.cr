f3bf41ba2cf350405d80158f798652ab1aa80203
Holden Omans <holden.omans@gmail.com>
2021-09-03 15:13:03 -0400

    removed kemal generator

--- src/spoved/kemal/routes/epidote_gen.cr
+++
@@ -1,3 +0,0 @@
-@[Deprecated("Route generator for Epidote ORM is deprecated. Please switch to Granite ORM")]
-macro epidote_gen_routes(model, path, filter = nil, id_class = UUID, formatter = nil, schema = nil)
-end
